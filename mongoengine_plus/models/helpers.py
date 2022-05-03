095b9510d45e99ad4b08a97c4cbc27b67ec6cd56
AlexViquez <alexviquez22@gmail.com>
2022-05-03 18:26:32 -0500

    EmbeddedDocument

--- mongoengine_plus/models/helpers.py
+++ mongoengine_plus/models/helpers.py
@@ -64,6 +64,8 @@ def mongo_to_dict(obj, exclude_fields: list = None) -> dict:
             return_data[field_name] = list_field_to_dict(data)
         elif isinstance(obj._fields[field_name], EmbeddedDocumentField):
             return_data[field_name] = mongo_to_dict(data, [])
+        elif isinstance(obj._fields[field_name], EmbeddedDocument):
+            return_data[field_name] = mongo_to_dict(data, [])
         elif isinstance(obj._fields[field_name], DictField):
             return_data[field_name] = data
         elif isinstance(obj._fields[field_name], EnumField):
