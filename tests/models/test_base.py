aa5b586b5fb4a38114d79e1d8849c29ded74fa8f
Roger <rogelio.lpz94@gmail.com>
2025-09-08 17:09:45 -0600

    fix doc

--- tests/models/test_base.py
+++ tests/models/test_base.py
@@ -1,17 +1,61 @@
-from mongoengine import Document, StringField
+from mongoengine import (
+    Document,
+    EmbeddedDocument,
+    EmbeddedDocumentField,
+    StringField,
+)
 
 from mongoengine_plus.models import BaseModel
 
 
+class File(EmbeddedDocument):
+    url = StringField()
+    file_type = StringField()
+
+
+class Address(EmbeddedDocument, BaseModel):
+    # Inheritance from BaseModel, to use _excluded and _hidden
+    _excluded = ['reference']
+    _hidden = ['secret_code']
+    street = StringField()
+    secret_code = StringField()
+    reference = StringField()
+
+
 class TestModel(BaseModel, Document):
-    id = StringField()
+    id = StringField(primary_key=True)
     secret_field = StringField()
+    address = EmbeddedDocumentField(Address)
+    document = EmbeddedDocumentField(File)
+
     __test__ = False
     _hidden = ['secret_field']
 
 
 def test_hide_field():
-    model = TestModel(id='12345', secret_field='secret')
+    address = Address(
+        street='123 Main St',
+        reference='not display this',
+        secret_code='898612',
+    )
+    doc = File(url='https://example.com', file_type='pdf')
+    model = TestModel(
+        id='12345',
+        secret_field='secret',
+        document=doc,
+        address=address,
+    )
     model_dict = model.to_dict()
-    assert model_dict['secret_field'] == '********'
-    assert model_dict['id'] == '12345'
+    expected = {
+        'id': '12345',
+        'secret_field': '********',
+        'address': {
+            'street': '123 Main St',
+            'secret_code': '********',
+        },
+        'document': {
+            'url': 'https://example.com',
+            'file_type': 'pdf',
+        },
+    }
+    assert model_dict == expected
